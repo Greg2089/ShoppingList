dc126735531afffd0476cbde0e7f37efa58886c0
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2024-03-18 12:16:09 +0300

    1)Data class, UseCases, Repository

---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,10 @@
+package com.example.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
+    fun getShopItem(shopItemId: Int): ShopItem
+    fun getShopList(): List<ShopItem>
+}
